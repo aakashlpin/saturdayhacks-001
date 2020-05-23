36dd901b2c6046ae342c8bdec6d930c7935a95b2
Aakash Goel <aakash.lpin@gmail.com>
2020-05-23 13:54:15 +0530

    the airtable hack

---
+++ README.md
@@ -0,0 +1,8 @@
+# [Saturday Hacks] 001
+Yay! You made it here!
+
+## The Airtable Hack!
+Hack your Airtable account with unlimited dollars in credits!
+
+### By attending, you abide by the following:
+The contents of this workshop include hacks that may cause service disruption or account closure of your Airtable account. If anything were to happen with your Airtable account because of this hack, I Aakash Goel cannot be held legally or morally responsible for it. This is purely an educational exercise and does not recommend credit mining as an activity. By attending this session, you agree to the above, and in case not, you are free to leave the session right away. By attending this session, it's implied that you abide by the aforesaid statement and agree not to indulge in credit mining for personal or commercial activities and abide by the Terms and Conditions mentioned on Airtable.com
---
+++ index.js
@@ -0,0 +1,114 @@
+const puppeteer = require('puppeteer');
+const faker = require('faker');
+const axios = require('axios');
+
+const format = (str) => str.replace(/[^a-zA-Z ]/g, '').toLowerCase();
+
+function userGenerator() {
+  const firstname = faker.name.firstName();
+  const lastname = faker.name.lastName();
+  const emailId = `${format(firstname)}.${format(lastname)}@dont.travel`;
+  const password = faker.internet.password();
+  return {
+    firstname,
+    lastname,
+    password,
+    emailId,
+  };
+}
+
+async function waitForEmailVerificationLink(emailId, maxWaitMs = 120 * 1000) {
+  return new Promise((resolve, reject) => {
+    async function fn() {
+      const requestUrl = `https://emailapi.io/api/otp`;
+      const params = {
+        email: emailId,
+        search_query: 'from: noreply@airtable.com subject: (Please confirm your email)',
+        configurations: [
+          {
+            fields: [
+              {
+                selector:
+                  'table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > a:nth-child(4)',
+                fieldName: 'verify',
+                fieldKey: 'verify',
+              },
+            ],
+          },
+        ],
+      };
+
+      const timer = setInterval(async () => {
+        const response = await axios.post(requestUrl, params);
+        const { data } = response;
+        switch(data.status) {
+          case 0: {
+            console.log('email not recevied yet');
+            break;
+          }
+          case 1: {
+            console.log('email was received but could not extract the data');
+            reject(new Error('email was received but could not extract the data'));
+            break;
+          }
+          case 2: {
+            console.log('email has been received');
+            const { results } = data;
+            const { verify_link: verifyLink } = results[0];
+            resolve(verifyLink);
+            clearInterval(timer);
+            break;
+          }
+          default:
+            break;
+        }
+      }, 8 * 1000);
+    }
+
+    fn();
+  })
+}
+
+async function doSignup() {
+  const browser = await puppeteer.launch(
+    {headless: false}
+  );
+  const page = await browser.newPage();
+  await page.goto('https://airtable.com/invite/r/Vl6Kw5S3');
+
+  const { firstname, lastname, password, emailId} = userGenerator();
+
+  await page.type('#sign-up-form-fields-root [name="firstName"]', firstname);
+  await page.type('#sign-up-form-fields-root [name="lastName"]', lastname);
+  await page.type('#sign-up-form-fields-root [name="email"]', emailId);
+  await page.type('#sign-up-form-fields-root [name="password"]', password);
+
+  await page.$eval('#signUpForm', form => form.submit());
+
+
+  // there's gonna be a verification email from airtable
+  // we have a configuration of that email
+  // is an API that can give the link in the verification email
+
+
+  try {
+    const verifyLink = await waitForEmailVerificationLink(emailId);
+    console.log({verifyLink});
+    await page.goto(verifyLink);
+  } catch(e) {
+    console.log(e);
+  }
+
+  // await page.waitFor(60 *1000);
+
+  await browser.close();
+}
+
+(async () => {
+  // bootstrap
+  [...new Array(24)]
+  .reduce(async (previousPromise) => {
+    await previousPromise;
+    await doSignup();
+  }, Promise.resolve())
+})();
